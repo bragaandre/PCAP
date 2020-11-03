82b88ea38b5320e4ece22ed77bd2ba39b01b7bc3
mastersmaz <mastersmaz@linux-lkse.lan>
2020-11-03 22:08:13 +0000

    Operations Module

--- numbers.py
+++ numbers.py
@@ -1,9 +1,13 @@
 ##Test with numbers
 
-print("2")
-print(2)
+print("2") #String
+print(2)   #Integer
 print(0o123)
 print(0x123)
 
 print(3E8)
 print(0.000000000000000000000001)
+
+#Boolean Example
+print(True>False)
+print(True<False)
---
+++ operations.py
@@ -0,0 +1,16 @@
+##Simple operations in Python
+
+print(2+2) #SUM
+print(2-2) #Subtraction
+print(2*2) #Multiplication
+print(2/2) #Division
+print(2//2) #Integer Divisional - results are always rounded
+print(14%2) #Modulo 
+print(2**2) #
+print(+2)
+
+
+##Exponention
+print(2**3) ## ** is the code for exponention base ** ex
+print(2.**3)
+print(2.**3.)
