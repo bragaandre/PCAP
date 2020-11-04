86c55939d4d0a2ef54aa44d4b20bfb42374351ba
bragaandre <andresbraga@ua.pt>
2020-11-04 15:50:24 +0000

    Exercise and Exp

---
+++ Ex-2.1.3.10.py
@@ -0,0 +1,13 @@
+##Exercise 2.1.3.10
+
+print('Example')
+print((2*2),(2%3),(10//2))
+
+print('Exercise 1')
+print((2 ** 4), (2 * 4.), (2 * 4))
+
+print('Exercise 2')
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+
+print('Exercise 3')
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
---
+++ exponention.py
@@ -0,0 +1,7 @@
+##Exponention examples
+
+
+print(2**3**2)
+
+print('Exercise 2.3.1.9')
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
