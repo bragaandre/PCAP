362ed3438e19c68e93f7245a5ad2f0340a9705f9
bragaandre <andresbraga@ua.pt>
2020-11-04 18:43:48 +0000

    pythagoras theorem

---
+++ pitagoras.py
@@ -0,0 +1,8 @@
+###Simple program to test the Pythagoras Theorem
+
+leg_a = float(input("Input first Leg length:"))
+leg_b = float(input("Input second Leg length:"))
+hypo = (leg_a**2+leg_b**2)**0.5
+
+print("Hypotenuse length is : ", hypo)
+
