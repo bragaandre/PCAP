d81aa2814efd00b2a12aff403017ce6e0113b14e
bragaandre <andresbraga@ua.pt>
2020-11-04 16:43:45 +0000

    Update README and  new shortcuts file

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Course PCAP -Programming Essentials in Python
 
 -- Snake.py - program test for first impression
+
+Updated in: November 4 2020
+
+
+
---
+++ shortcuts.py
@@ -0,0 +1,16 @@
+### Tests with Python Shortcuts
+
+#Variables initialization
+i = 1
+var = 3
+rem = 6
+x = 0
+j=0
+
+i += 2 * j                  ## i = i + 2 * j
+var /= 2                    ## var = var / 2
+rem %= 10                   ## rem = rem % 10
+j -= (i + var + rem)        ## j = j - (i + var + rem)
+x**=2                       ## x= x**2
+
+print(j+x)
