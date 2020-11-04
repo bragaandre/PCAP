2a3bfc3ed0bd839baecc2062ff13b1c0cae8cb43
bragaandre <andresbraga@ua.pt>
2020-11-04 18:06:56 +0000

    exercises w/input

---
+++ converter.py
@@ -0,0 +1,10 @@
+### Simple converter KM_to_Miles
+
+km = 12.25       #Kilometers
+miles= 7.38       #Miles 
+
+km_to_miles = km / 1.609344
+miles_to_km = miles * 1.609344
+
+print(miles, " Miles are", round(miles_to_km,2), "Kilometers." ) 
+print(km, "Kilometers are", round(km_to_miles,2), "Miles.")
---
+++ exinput.py
@@ -0,0 +1,9 @@
+##Exercise with Input 
+
+anything = input("Enter a number: ")
+
+anything = float(anything)
+
+something = anything ** 2.0
+
+print(anything, "to the power of 2 is", something)
