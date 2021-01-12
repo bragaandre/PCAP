05d4c97c03c77d001b9db9d298493530d596f6e9
bragaandre <andresbraga@ua.pt>
2021-01-12 13:05:23 +0000

    Module 3.1.1.3

---
+++ 3113.py
@@ -0,0 +1,21 @@
+###
+### 3.1.1.3 Module for testing "=="
+
+var = 0 # Assigning 0 to var
+print(var == 0)
+
+var = 1 # 1 to var
+print(var == 0)
+
+### Test "!=" the not equal operator
+
+var = 0 
+print(var!=0)
+
+
+var = 1 
+print(var != 0)
+
+
+
+
