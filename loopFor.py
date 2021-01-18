50dca3aa079fd4c653a49a832a96c7eea3403c33
bragaandre <andresbraga@ua.pt>
2021-01-18 14:34:09 +0000

    Loop for&mississippi

---
+++ loopFor.py
@@ -0,0 +1,8 @@
+####Module 3 - Ex:.3.1.2.4 Loops in Python
+
+
+for i in range(2,8):
+    print("The Value i is:", i)
+
+
+
---
+++ mississippi.py
@@ -0,0 +1,11 @@
+#### Module 3 - 3.1.2.6 LAB: Essentials of the for loop - counting mississippily
+
+import time ###import
+
+
+for i in range(1,6):
+    print(i,"Mississippi")
+    time.sleep(1)
+
+print("Ready or not, here i come!")
+
