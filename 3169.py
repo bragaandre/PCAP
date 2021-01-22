e705f4c9284794a8a68d8e002b446c78ffbf1851
bragaandre <andresbraga@ua.pt>
2021-01-22 14:34:14 +0000

    3.1.6.9

---
+++ 3169.py
@@ -0,0 +1,16 @@
+########################################
+### 3.1.6.9 LAB: Operating with lists###
+########################################
+
+
+myList = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+tempList = []
+
+myList.sort()
+tempList = myList
+
+for i in range(len(tempList
+
+print("The list with unique elements only:")
+
+print(myList)
