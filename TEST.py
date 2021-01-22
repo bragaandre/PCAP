0c2d521c76e1a30e5a5ad323b0d3210c7a932233
bragaandre <andresbraga@ua.pt>
2021-01-22 15:00:25 +0000

    End Module 3

---
+++ TEST.py
@@ -0,0 +1,10 @@
+
+
+lst = [0,1,2,3]
+
+x=1
+
+for elem in lst:
+    x*=elem
+
+print(x)
---
+++ cube.py
@@ -0,0 +1,25 @@
+# Cube - a three-dimensional array (3x3x3)
+
+cube = [[[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':(', 'x', 'x']],
+
+        [[':)', 'x', 'x'],
+         [':(', 'x', 'x'],
+         [':)', 'x', 'x']],
+
+        [[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':)', 'x', 'x']]]
+
+print(cube)
+print(cube[0][0][0]) # outputs: ':('
+print(cube[2][2][0]) # outputs: ':)'
+
+for i in range(-1,1):
+    print('#')
+
+
+lst = [3,1,-1]
+lst[-1]=lst[-2]
+print(lst)
