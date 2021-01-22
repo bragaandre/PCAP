07a8b087fb0e5c9fd94bd545c28aedfd1094fe51
bragaandre <andresbraga@ua.pt>
2021-01-22 14:07:40 +0000

    Lists

---
+++ 3166.py
@@ -0,0 +1,11 @@
+#####
+#####3.1.6.6 - in, not in
+#####
+
+
+myList = [2,4,12,6,8,21]
+
+print(5 in myList)
+print(5 not in myList)
+print(12 in myList)
+
---
+++ 3167.py
@@ -0,0 +1,11 @@
+####3.1.6.7 Lists
+
+
+myList = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = myList[0]
+
+for i in myList[1:]:
+    if i > largest:
+        largest = i
+
+print(largest)
