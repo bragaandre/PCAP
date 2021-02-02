8cb610b293812e709112f6aebb9665b904469042
root <root@linux-lkse.home>
2021-02-02 18:51:36 +0000

    Upload 4.1 ex

---
+++ 4131.py
@@ -0,0 +1,15 @@
+####
+#### Exercise 4.1.3.1###
+
+
+def happy_new_year(wishes=True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+
+
+happy_new_year(False) 
+print("Happy New Year!")
+
---
+++ squareroot.py
@@ -0,0 +1,15 @@
+####Square root simple program"""
+
+import math
+
+
+
+def sqrt(x):
+    return math.sqrt(x)
+
+
+number = float(input("Square Root of:"))
+
+print(sqrt(number))
+
+
