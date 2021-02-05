e272ba2cc6f0c111837aab099a7122c5658f84f5
root <root@linux-lkse.home>
2021-02-05 15:24:12 +0000

    LAB4136 exec

---
+++ LAB4136.py
@@ -0,0 +1,30 @@
+
+def isYearLeap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def daysInMonth(year, month):
+    
+
+
+
+testYears = [1900, 2000, 2016, 1987]
+testMonths = [2, 2, 1, 11]
+testResults = [28, 29, 31, 30]
+for i in range(len(testYears)):
+	yr = testYears[i]
+	mo = testMonths[i]
+	print(yr, mo, "->", end="")
+	result = daysInMonth(yr, mo)
+	if result == testResults[i]:
+		print("OK")
+	else:
+		print("Failed")
